280393a013b88a97f3d8abe8d615dd448c3c9e35
たのけん <tanoken729@gmail.com>
2021-07-01 22:44:38 +0900

    create validation using form request

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\CreateUserRequest;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,14 +14,14 @@ class RegisterController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
+    public function create(CreateUserRequest $request)
     {
         // モデルからインスタンスを生成
         $user = new User;
         // $requestにformからのデータが格納されているので、以下のようにそれぞれ代入する
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
         // 保存
         $user->save();
     }
