291d24cfc671be0af3c33ce2b45bd526aadb961f
たのけん <tanoken729@gmail.com>
2021-07-12 23:29:43 +0900

    アップロードしたファイルにアクセスするためfile関数を追記

--- src/app/Http/Controllers/MusicFileController.php
+++ src/app/Http/Controllers/MusicFileController.php
@@ -17,10 +17,10 @@ class MusicFileController extends Controller
     {
         $music_file = new MusicFile;
         // music_fileにファイル名をつけて保存
-        $file_name = $request->music_file->getClientOriginalName();
+        $file_name = $request->file('music_file')->getClientOriginalName();
         $music_file->music_file = Storage::putFileAs('public',$request->music_file, $file_name);
         // cover_imagにファイル名をつけて保存
-        $file_name = $request->cover_image->getClientOriginalName();
+        $file_name = $request->file('cover_image')->getClientOriginalName();
         $music_file->cover_image = Storage::putFileAs('public',$request->cover_image, $file_name);
         $music_file->title = $request->title;
         $music_file->genre = $request->genre;
