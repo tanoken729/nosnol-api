136595607373f847385a001c1748b417370a5aaf
たのけん <tanoken729@gmail.com>
2021-09-03 00:41:07 +0900

    コメント時も初期表示と同様にデータ表示させる

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -16,11 +16,13 @@ class CommentController extends Controller
         $comment_info = DB::table('music_files')
                 ->where('music_files.id', '=', $music_file_id)
                 ->leftJoin('comments', 'comments.music_file_id', '=', 'music_files.id')
-                // ->leftJoin('users', 'comments.user_id', '=', 'users.id')
-                // ->leftJoin('users', function ($join) use (){
-                //     $join->on('users.id', '=', 'comments.user_id')
-                //         ->where('likes.user_id', '=', $user_id);
-                // })
+                ->leftJoin('users as commenter', 'commenter.id', '=', 'comments.user_id')
+                ->select(
+                    'text',
+                    'comments.user_id as comments_user_id',
+                    'comments.created_at as comments_created_at',
+                    'commenter.name as commenter_name'
+                    )
                 ->get();
         return response()->json(['commentInfo' => $comment_info]);
     }
